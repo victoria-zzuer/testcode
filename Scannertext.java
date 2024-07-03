5effeb60573c64d0ac90213c3ff304063bcb645e
victoria <165569497+victoria-zzuer@users.noreply.github.com>
2024-07-03 10:36:59 +0800

    更改多网址
    尝试一次上传多个文件

---
+++ Scannertext.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Scannertext{
+	public static void main(String[] args){
+
+		Scanner sc=new Scanner(system.in);
+		System.out.println("请输入第一个数字");
+		int number1=sc.nextInt();
+		System.out.println("请输入第二个数字");
+		int number2=sc.nextInt();
+		System.out.println(number1+number2);
+	}
+}
